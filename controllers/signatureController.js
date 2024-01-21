bf695962911b479a552130759cad4bb9949013ac
swapnilabane <swapnil143a@gmail.com>
2024-01-21 19:06:11 +0530

    18 commit

--- controllers/signatureController.js
+++ controllers/signatureController.js
@@ -20,10 +20,25 @@ const generateSignature = (req, res, next) => {
   try {
     const timestamp = Math.round(new Date().getTime() / 1000);
 
+    // Determine the upload preset based on the folder
+    const uploadPreset =
+      folder === 'images'
+        ? process.env.CLOUDINARY_IMAGES_UPLOAD_PRESET
+        : folder === 'videos'
+        ? process.env.CLOUDINARY_VIDEOS_UPLOAD_PRESET
+        : null;
+
+    if (!uploadPreset) {
+      res.status(400);
+      return next(new Error('Invalid folder specified'));
+    }
+
+    // Include the upload preset in the parameters
     const signature = cloudinary.utils.api_sign_request(
       {
         timestamp,
         folder,
+        upload_preset: uploadPreset,
       },
       process.env.CLOUDINARY_API_SECRET
     );
